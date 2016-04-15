fa2520bb0e6dc14f4ddaee085e8a85717cba849b
Alexander Apchiory <apchi877@gmail.com>
2016-04-15 12:42:33 +0300

    small changes

--- routes/gameScreen.js
+++ routes/gameScreen.js
@@ -5,4 +5,10 @@ router.get('/:gameid', function(req, res, next) {
   res.render('gamescreen', {});
 });
 
+
+// TODO - delete this!!!
+router.get('/', function(req, res, next) {
+  res.render('gamescreen', {});
+});
+
 module.exports = router;
