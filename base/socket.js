ca6b53954862be795450528f60e98c738ebc6287
Alexander Apchiory <apchi877@gmail.com>
2016-04-17 16:35:15 +0300

    The numbers color goes :back to gray on disconnect

--- base/socket.js
+++ base/socket.js
@@ -1,10 +1,16 @@
 module.exports = function(io){
   io.on('connection', function(socket){
+    var numSer = 0;
     console.log("User connected!");
 
     socket.on('selected', function(num){
-      console.log("recieved: "+num);
+      numSer = num;
       socket.broadcast.emit('selectednum', num);
     });
+
+    socket.on('disconnect', function(){
+      console.log(numSer);
+      socket.broadcast.emit('notanswered', numSer);
+    })
   });
 };
--- public/javascripts/game/game-socket.js
+++ public/javascripts/game/game-socket.js
@@ -1,7 +1,15 @@
+//TODO remmeber to minify the file before you check it!
+
 var socket = io();
 
 socket.on('selectednum', function(num){
   console.log('reciecved: '+num);
   $('#num'+num).removeClass("notanswered");
   $('#num'+num).addClass("selected");
-})
+});
+
+socket.on('notanswered', function(num){
+  console.log("notanswered: "+num);
+  $('#num'+num).removeClass("selected");
+  $('#num'+num).addClass("notanswered");
+});
