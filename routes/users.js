9a01140938c79737e4dfbcf960fd9abe9df0fc9f
yael dodeles <yael dodeles>
2016-06-05 09:55:19 +0300

    fix users js

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-//        /users
-
-/* GET users listing. */
-
 router.get('/profile', function(req, res, next) {
   res.render('profile/profilescreen');
+});
 
 router.get('/', function(req, res, next) {
    res.send('respond with a resource');
