11e7141ab165312691885723d718bdc99de87d03
Alexander Apchiory <apchi877@gmail.com>
2016-07-02 20:44:31 +0300

    ques list changes

--- public/javascripts/questions/quelist/controller.js
+++ public/javascripts/questions/quelist/controller.js
@@ -6,25 +6,23 @@ angular.module("mainApp").controller('que-list-ctrl', ['$scope', '$http', functi
          { field: "answer", displayName: "Answer" },
          { field: "isPrivate", displayName: "Public" },
          { field: "nickname", displayName: "Created By" }
-      ]
-   }
+      ],
+      enableGridMenu: true,
+      onRegisterApi: function(gridApi) {
+         $scope.gridApi = gridApi;
+      }
+   };
 
-   $scope.getQuestions = function(){
-      $http.get('/questions/list/public').then(function(result){
+   $scope.getQuestions = function(type){
+      $http.get('/questions/list/'+type).then(function(result){
          $scope.gridOptions.data = result.data;
       });
+   };
+
+   $scope.goToAddQuestion = function(){
+      window.location.href = "/questions/add";
    }
 
-
-   $scope.getQuestions();
+   $scope.getQuestions('public');
 
 }]);
-
-//=============== Private Methods ===============
-
-function isArrayContains(array, item){
-   for(i=0;i<array.length;i++){
-      if(item == array[i]) return true;
-   }
-   return false;
-}
